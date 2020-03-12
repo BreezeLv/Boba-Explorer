088c024d2623d734505feb961c13eff5f2bd8cfe
Bernard Lyu <breezelv1999@gmail.com>
2020-03-12 18:19:25 -0500

    Add Icon; Change Title

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer>
+        Icons made by <a href="https://www.flaticon.com/authors/pause08" title="Pause08">Pause08</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </footer>
     </div>
   );
 }
