ca1da10f1d9536e5eec2934a98b8ba16f33afa5b
Bernard Lyu <breezelv1999@gmail.com>
2020-04-03 18:00:03 -0500

    Add template for user registration/login route

--- server/server.py
+++ server/server.py
@@ -1,3 +1,8 @@
+# Dependencies: Flask, flask-cors
+# To install Flask: `pip install Flask`
+# To install flask-cors: `pip install -U flask-cors`
+# Follow their offical documentation for details
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -5,14 +10,38 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root():
+    return 'SERVER ALIVE'
 
 @app.route('/search')
 def search():
     query = request.args.get('q')
     return {"echo" : query}
 
+@app.route('/register', methods=['POST'])
+def register():
+    req_body = request.json()
+    username = req_body['username']
+    password = req_body['password']
+    email = req_body['eamil']
+    # created_date = ddmmyy
+
+    # TODO: If username, password all valid, create a new user, insert proper
+    # entries to the database
+    # Otherwise return some error message or error code
+
+    return {'uid':12345}
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    req_body = request.json()
+    username = req_body['username']
+    password = req_body['password']
+    
+    # TODO: If username, password all valid, log user in
+    # Otherwise return some error message or error code
+
+    return {'uid':12345}
 
 if __name__ == '__main__':
     app.run(debug=True)
