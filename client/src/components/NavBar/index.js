07a30dcd2ad453eb5f4e50781fcaa8c2b544a28e
Bernard Lyu <breezelv1999@gmail.com>
2020-04-04 00:00:45 -0500

    Add logo; Add semanticUI; Add login button group

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import './index.css';
+import styled from 'styled-components';
+import { Button } from 'semantic-ui-react';
 
 function NavBar() {
+
+  const FlexContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 0 0;
+    font-size: 12px;
+	  font-family: sans-serif;
+    background: #2c3e50;
+  `;
+
   return (
-    <div className='navbar-container'>
-        <nav>
-            <a href="/">Home</a>
-            <a href="explore">Explore</a>
-            <a href="review">Review</a>
-            <a href="community">Community</a>
-            <a href="contact">Contact</a>
-            <div className="animation"></div>
-        </nav>
-    </div>
+    <FlexContainer>
+
+      <div className='logo-container'>
+        <a href='/'>
+          <img className='navbar_logo' src='logo192.png' alt='logo'/>
+        </a>
+      </div>
+
+      <div className='navbar-container'>
+          <nav>
+              <a href="/">Home</a>
+              <a href="explore">Explore</a>
+              <a href="review">Review</a>
+              <a href="community">Community</a>
+              <a href="contact">Contact</a>
+              <div className="animation"></div>
+          </nav>
+      </div>
+
+      <div className='login-container'>
+        <Button inverted color='blue' onClick={()=>console.log("someone wants to login")}>Log In</Button>
+        <Button inverted color='olive' onClick={()=>console.log("someone wants to login")}>Register</Button>
+      </div>
+
+    </FlexContainer>
   );
 }
 
