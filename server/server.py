3fade3a45a764157c24bb3d33898a7c674886e47
Bernard Lyu <breezelv1999@gmail.com>
2020-04-07 16:54:56 -0500

    Fix bugs in reg/login

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@
 # To create user: run `CREATE USER 'x86'@'localhost' IDENTIFIED BY 'x86x86';` as root
 # To grant privilege: run `GRANT ALL PRIVILEGES ON boba.* TO 'x86'@'localhost';` as root
 
-from flask import Flask, request, jsonify, redirect, session, flash
+from flask import Flask, request, jsonify, redirect, session
 from flask_cors import CORS
 import pymysql
 import simplejson as json
@@ -41,54 +41,49 @@ def search():
     return dict
 
 @app.route('/register', methods=['POST'])
-def register():    
-    if request.method == "POST":      
-        req_body = request.json        
-        username = req_body['username']        
-        password = req_body['password']        
-        email = req_body['email']    
-        # created_date = ddmmyy
+def register():       
+    req_body = request.json
+    username = req_body['username']
+    password = req_body['password']
+    email = req_body['email']
+    # created_date = ddmmyy
 
-        cur = conn.cursor()
-        userid_sql = "select count(*) from user"
-        cur.execute(userid_sql)
-        user_id = cur.fetchall()
-        cur.execute("insert into user(user_id, user_name, password, email) VALUES(%s,%s,%s,%s)", (int(user_id), str(username), str(password), str(email)))
-        # commit to DB
-        # mysql.connection.commit()
-        # close connection
-        # cur.close()
-        # flash("You are now Registered and you can login" , 'success')
-        # redirect(url_for('login'))
-        
-    return {'uid':12345}
+    cur = conn.cursor()
+    userid_sql = "select count(*) as cnt from user;"
+    cur.execute(userid_sql)
+    user_id = cur.fetchone()['cnt']
+    result = cur.execute("insert into user(user_id, user_name, password, email) VALUES(%s,%s,%s,%s)", (int(user_id), str(username), str(password), str(email)))
+    # commit to DB
+    conn.commit()
+    # close connection
+    cur.close()
+
+    if result: return {'user_id':user_id}
+    else: return {'err_msg':'Unable to register the user!'}
 
 
 @app.route('/login', methods=['POST'])
-def signin():    
-    if request.method == "POST":        
-        req_body = request.json        
-        email = req_body['email']        
-        password = req_body['password']
+def signin():
+    req_body = request.json
+    email = req_body['email']
+    password = req_body['password']
 
-        cur = conn.cursor()        
-        result = cur.execute("SELECT * FROM users WHERE email = %s" ,[email])
-        if result > 0 :            
-            data = cur.fetchone()            
-            password_db = data['password']            
-            if password == password_db:                
-                session['logged_in'] = True                
-                # session['username'] = username                
-                flash('You are now logged in ','success')
-                # return redirect(url_for('dashboard'))                
-                return {'uid':12345}            
-            else:                
-                flash("Incorrect password")                
-                # return redirect("/login")                
-                return {'uid':12345}
-        else:           
-            error = 'Username not found'    
-    return {'uid':12345}
+    cur = conn.cursor()
+    result = cur.execute("SELECT * FROM user WHERE email = %s" ,[email])
+    if result > 0 :
+        data = cur.fetchone()
+        password_db = data['password']
+        uid = data['user_id']
+        username = data['user_name']
+
+        if password == password_db:
+            # session['logged_in'] = True
+            # session['username'] = username
+            return {'user_id':uid, 'username':username}
+        else:              
+            return{'err_msg':"Incorrect Password"}
+    else:
+        return {'err_msg':'Incorrect Username'}
 
 if __name__ == '__main__':
     app.run(debug=True)
