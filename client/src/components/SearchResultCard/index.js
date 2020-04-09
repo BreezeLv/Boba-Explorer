319eee5bbe832f2605b7146e509608d2ae7a5b71
Bernard Lyu <breezelv1999@gmail.com>
2020-04-09 17:35:32 -0500

    Add reviewing text area; Add transistions between operations

--- client/src/components/SearchResultCard/index.js
+++ client/src/components/SearchResultCard/index.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import { Button, Item } from 'semantic-ui-react';
+import { Button, Item, TextArea, TransitionGroup } from 'semantic-ui-react';
 
-function SearchResultCard(props) {
-    return (
-        <>
-            <Item>
-                <Item.Image size='small' src='/logo192.png' />
+class SearchResultCard extends React.Component {
 
-                <Item.Content verticalAlign='middle'>
-                    <Item.Header>{props.product.product_name ? props.product.product_name : 'Unknown Product'}</Item.Header>
-                    <Item.Meta>{props.product.size ? props.product.size : 'Regular'}</Item.Meta>
-                    <Item.Description>
-                        ${props.product.price ? props.product.price : 'Unknown Price'}
-                    </Item.Description>
-                    <Item.Extra as='a'>From teamoji</Item.Extra>
-                    <Item.Extra><Button color='teal' style={{marginRight:0}}>Write a review</Button></Item.Extra>
-                </Item.Content>
-            </Item>
-        </>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            commentVisible : false,
+        };
+    }
+
+    CommentSwitch = () => {
+        this.setState((prevState)=>{return {commentVisible:!prevState.commentVisible}});
+    }
+
+    render() {
+        const {product} = this.props;
+
+        return (
+            <>
+                <Item>
+                    <Item.Image size='small' src='/logo192.png' />
+
+                    <Item.Content verticalAlign='middle'>
+                        <Item.Header>{product.product_name ? product.product_name : 'Unknown Product'}</Item.Header>
+                        <Item.Meta>{product.size ? product.size : 'Regular'}</Item.Meta>
+                        <Item.Description>
+                            ${product.price ? product.price : 'Unknown Price'}
+                        </Item.Description>
+                        <Item.Extra as='a'>From teamoji</Item.Extra>
+                        
+                        <TransitionGroup animation='swing down' duration={{hide:400,show:900}}>
+                        {!this.state.commentVisible && (<Item.Extra><Button color='teal' style={{marginRight:0}} onClick={this.CommentSwitch} >Write a review</Button></Item.Extra>)}
+                        </TransitionGroup>
+
+                        <TransitionGroup animation='slide down' duration={{hide:300,show:900}}>
+                            {this.state.commentVisible && (
+                                <div>
+                                    <Item.Extra>
+                                        <TextArea placeholder='Leave something..'></TextArea>
+                                    </Item.Extra>
+                                    <Item.Extra>
+                                        <Button color='red' onClick={this.CommentSwitch} >Cancel</Button>
+                                        <Button color='green' onClick={this.CommentSwitch} >Submit</Button>
+                                    </Item.Extra>
+                                </div>
+                            )}
+                        </TransitionGroup>
+                        
+                    </Item.Content>
+                </Item>
+            </>
+        );
+    }
 }
 
 export default SearchResultCard;
